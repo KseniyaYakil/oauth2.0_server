2ecaf35d670fe2d2734623c1a196dc9275681cf0
Kseniya Yakil <k.yakil@yandex.ru>
2014-12-14 19:53:43 +0300

    in processing auth code request: - added checking client id in db - added checking redirect_uri

--- oauth2_server/auth_manager/views.py
+++ oauth2_server/auth_manager/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from config import AuthConf
 from django.http.response import HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login
+from .models import client_info, access_token
+from urlparse import urlparse
 
 #access_token request
+
+def check_client_id(client_app_id):
+		try:
+				client_app = get_object_or_404(client_info, client_id=client_app_id)
+				return client_app
+		except DoesNotExist:
+				print("ERR: client with client_id {0} doesn't exist").format(client_app_id)
+				return None
 
 #TODO: add 'state' parametr 
 @csrf_exempt
@@ -28,32 +38,38 @@ def auth_code_req(request):
 						auth_need_params[key] = request.GET[key]
 
 				#check client_id in data base
+				client_app = check_client_id(auth_need_params['client_id'])
+				if client_app is None:
+						return redirect('{0}?error=no_client'.format(auth_need_params['client_id']))
+				auth_need_params['client_name'] = client_app.client_name
 
 				#check redirect uri
-				#TODO: rename app_name with real application name
+				if urlparse(auth_need_params['redirect_uri']).netloc != urlparse(client_app.redirect_domain).netloc:
+						print "ERR: no specified domain in db"
+						return redirect("{0}/?error=incorrect_domain".format(auth_need_params['redirect_uri']))
+
 				if request.user.is_authenticated():
 						print "user authenticated. request access"
-						return render(request, 'auth_manager/access.html', {'app_name' : 'test_app'})
+						return render(request, 'auth_manager/access.html', {'app_name' : client_app.client_name})
 				else:
+						print "ask for logging in"
 						request.session['redirect_internal'] = '/auth'
 						request.session['stored_auth_params'] = auth_need_params
 						return redirect('/user/login_user/')
 
+		auth_need_params = request.GET if has_auth_params == 0 else request.session['stored_auth_params']
+
 		if request.method == 'GET' and has_auth_params and request.user.is_authenticated():
-				print "user authenticated. request access"
-				return render(request, 'auth_manager/access.html', {'app_name' : 'test_app'})
+				return render(request, 'auth_manager/access.html', {'app_name' : auth_need_params['client_name']})
 
 		for param  in request.POST.items():
 				print("post : {0}").format(param)
+		for param  in auth_need_params.items():
+				print("get : {0}").format(param)
 
-		if has_auth_params:
-				for param  in request.session['stored_auth_params'].items():
-						print("get params: {0}").format(param)
-		else:
-				for param  in request.GET.items():
-						print("get : {0}").format(param)
 
 		request.session['stored_auth_params'].clear()
+		#TODO: set correctly variable 'has_access' in form
 		if 'has_access' not in request.POST:
 				print "ERR: no field `has access'"
 				raise Http404
